5d234d49f83d786c4638c5ceec8e2f06883f74b4
yagmurbrnk <yagmurbarankarakus@outlook.com>
2025-02-03 17:47:10 +0300

    Add 'department' field to User entity

--- src/main/java/com/ybk/hr_management/model/User.java
+++ src/main/java/com/ybk/hr_management/model/User.java
@@ -16,6 +16,7 @@ public class User {
     private Long id;
     private String name;
     private String lastName;
+    private String department;
     private String username;
     private String password;
     private String role;
