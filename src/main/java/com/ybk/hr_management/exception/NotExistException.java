e39b3d0ff91b398630722a8c3f75d448feb66e74
yagmurbrnk <yagmurbarankarakus@outlook.com>
2025-02-26 18:53:12 +0300

    update: `NotExistException`

--- src/main/java/com/ybk/hr_management/exception/NotExistException.java
+++ src/main/java/com/ybk/hr_management/exception/NotExistException.java
@@ -2,12 +2,12 @@ package com.ybk.hr_management.exception;
 
 import java.io.Serial;
 
-public class NotExistException extends Exception {
+public class NotExistException extends RuntimeException {
+
     @Serial
     private static final long serialVersionUID = -436261361729769744L;
 
     public NotExistException(final String message) {
         super(message);
     }
-
 }
